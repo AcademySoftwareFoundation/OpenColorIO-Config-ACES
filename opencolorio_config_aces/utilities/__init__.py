215a0bf28cc3bafeeec3ee278463837cecaebd1b
Thomas Mansencal <thomas.mansencal@gmail.com>
2021-08-27 16:53:52 +1200

    Made "jsonpickle" an optional dependency. (#29)
    Closes #28.
    
    Signed-off-by: Thomas Mansencal <thomas.mansencal@gmail.com>
    
    Co-authored-by: Michael Dolan <michdolan@gmail.com>

--- opencolorio_config_aces/utilities/__init__.py
+++ opencolorio_config_aces/utilities/__init__.py
@@ -1,16 +1,16 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright Contributors to the OpenColorIO Project.
 
-from .common import (DocstringDict, first_item, common_ancestor,
-                     paths_common_ancestor, vivification, vivified_to_dict,
-                     message_box, is_colour_installed, is_networkx_installed,
-                     is_opencolorio_installed, REQUIREMENTS_TO_CALLABLE,
-                     required, is_string, is_iterable, git_describe)
+from .common import (
+    DocstringDict, first_item, common_ancestor, paths_common_ancestor,
+    vivification, vivified_to_dict, message_box, is_colour_installed,
+    is_jsonpickle_installed, is_networkx_installed, is_opencolorio_installed,
+    REQUIREMENTS_TO_CALLABLE, required, is_string, is_iterable, git_describe)
 
 __all__ = [
     'DocstringDict', 'first_item', 'common_ancestor', 'paths_common_ancestor',
     'vivification', 'vivified_to_dict', 'message_box', 'is_colour_installed',
-    'is_networkx_installed', 'is_opencolorio_installed',
-    'REQUIREMENTS_TO_CALLABLE', 'required', 'is_string', 'is_iterable',
-    'git_describe'
+    'is_jsonpickle_installed', 'is_networkx_installed',
+    'is_opencolorio_installed', 'REQUIREMENTS_TO_CALLABLE', 'required',
+    'is_string', 'is_iterable', 'git_describe'
 ]
