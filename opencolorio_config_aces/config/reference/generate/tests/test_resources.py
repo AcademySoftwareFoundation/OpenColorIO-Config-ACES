348129703f5ae421be03c1f143769e67ce28fd93
Thomas Mansencal <thomas.mansencal@gmail.com>
2022-03-15 18:57:34 +1300

    Implement unit tests for the "CSV" mapping files.
    Signed-off-by: Thomas Mansencal <thomas.mansencal@gmail.com>

---
+++ opencolorio_config_aces/config/reference/generate/tests/test_resources.py
@@ -0,0 +1,63 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright Contributors to the OpenColorIO Project.
+"""
+Defines the unit tests for the
+:mod:`opencolorio_config_aces.config.reference.generate` module resources.
+"""
+
+
+import unittest
+import urllib.request
+
+from pathlib import Path
+
+__author__ = "OpenColorIO Contributors"
+__copyright__ = "Copyright Contributors to the OpenColorIO Project."
+__license__ = "New BSD License - https://opensource.org/licenses/BSD-3-Clause"
+__maintainer__ = "OpenColorIO Contributors"
+__email__ = "ocio-dev@lists.aswf.io"
+__status__ = "Production"
+
+__all__ = [
+    "TestConfigResources",
+]
+
+
+class TestConfigResources(unittest.TestCase):
+    """
+    Define the *aces-dev* reference *OpenColorIO* config resources unit tests
+    methods.
+    """
+
+    def test_csv_mapping_file(self):
+        """Test the *CSV* mapping file."""
+
+        csv_local_path = (
+            Path(__file__).parents[0]
+            / ".."
+            / "resources"
+            / "OpenColorIO-Config-ACES _Reference_ Transforms - "
+            "Reference Config - Mapping.csv"
+        )
+
+        with open(str(csv_local_path)) as csv_local_file:
+            csv_local_content = csv_local_file.read()
+
+        csv_remote_url = (
+            "https://docs.google.com/spreadsheets/d/"
+            "1SXPt-USy3HlV2G2qAvh9zit6ZCINDOlfKT07yXJdWLg/"
+            "export?format=csv&gid=273921464"
+        )
+
+        csv_remote_content = (
+            urllib.request.urlopen(csv_remote_url).read().decode("utf-8")
+        )
+
+        self.assertMultiLineEqual(
+            "\n".join(csv_remote_content.splitlines()),
+            "\n".join(csv_local_content.splitlines()),
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
