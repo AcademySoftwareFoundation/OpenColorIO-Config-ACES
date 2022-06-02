ff71a4166a623c319f0d78064c77e301504106b3
scoopxyz <sean@seancooper.xyz>
2022-06-02 22:26:48 +0200

    address CI tests
    Signed-off-by: scoopxyz <sean@seancooper.xyz>

--- opencolorio_config_aces/clf/transforms/arri/generate.py
+++ opencolorio_config_aces/clf/transforms/arri/generate.py
@@ -36,11 +36,11 @@ CLF_SUFFIX = ".clf"
 
 
 def generate_logc3(ei=800, clipping=True, debug=False):
-    """  Generate ARRI LogC3 CLF transforms.
+    """Generate ARRI LogC3 CLF transforms.
 
-    This method provides the connecting derivation between the public aces-dev IDT "v3_IDT_maker" code
-    and the "ALEXA Log C Curve Usage in VFX 09-Mar-17" White Paper parameters, which also conform to the
-    required CLF cameraLogToLin parameters.
+    This method provides the connecting derivation between the public aces-dev IDT
+    "v3_IDT_maker" code and the "ALEXA Log C Curve Usage in VFX 09-Mar-17" White Paper
+    parameters, which also conform to the required CLF cameraLogToLin parameters.
 
     Currently only EI values 1280 and below are supported.
 
@@ -62,7 +62,9 @@ def generate_logc3(ei=800, clipping=True, debug=False):
     """
 
     if not (160 <= ei <= 1280):
-        print("Error: Unsupported EI{:d} requested, must be 160 <= EI < 1600".format(ei))
+        print(
+            f"Error: Unsupported EI{ei:d} requested, must be 160 <= EI < 1600"
+        )
         return False
 
     # v3_IDT_maker.py parameters
@@ -72,7 +74,7 @@ def generate_logc3(ei=800, clipping=True, debug=False):
     encodingOffset = 400 / 1023  # was: 0.391007
     cutPoint = 1 / 9  # renamed from "cut"
     slope = 1 / (cutPoint * log(10))
-    offset = log10(cutPoint) - slope * cutPoint 
+    offset = log10(cutPoint) - slope * cutPoint
     gain = ei / nominalEI
     gray = midGraySignal / gain
     encGain = (log(gain) / log(2) * (0.89 - 1) / 3 + 1) * encodingGain
@@ -85,40 +87,47 @@ def generate_logc3(ei=800, clipping=True, debug=False):
     # derived "ALEXA Log C Curve Usage in VFX 09-Mar-17" style parameters
     # compatible with CLF cameraLogToLin parameters
     cut = (cutPoint - nz) * gray * (0.18 / (midGraySignal * nominalEI / ei))
-    a = 1.0 / (gray * 0.18 / (midGraySignal * nominalEI/ei))
+    a = 1.0 / (gray * 0.18 / (midGraySignal * nominalEI / ei))
     b = nz
     c = encGain
     d = encOffset
 
     # unused
-    e = c * ( a / ((a * cut + b) * log(10)))  # follows CLF 6.6 Log specification
-    f = (c * log10(a * cut + b) + d) - e * cut  # follows CLF 6.6 Log specification
+    e = c * (
+        a / ((a * cut + b) * log(10))
+    )  # follows CLF 6.6 Log specification
+    f = (
+        c * log10(a * cut + b) + d
+    ) - e * cut  # follows CLF 6.6 Log specification
 
     if debug:
-        print("White Paper Values for EI{:d}".format(ei))
-        print("cut: {:.6f} :: {:.15f}".format(cut, cut))
-        print("a:   {:.6f} :: {:.15f}".format(a, a))
-        print("b:   {:.6f} :: {:.15f}".format(b, b))
-        print("c:   {:.6f} :: {:.15f}".format(c, c))
-        print("d:   {:.6f} :: {:.15f}".format(d, d))
-        print("e:   {:.6f} :: {:.15f}".format(e, e))
-        print("f:   {:.6f} :: {:.15f}".format(f, f))
-
+        print(f"White Paper Values for EI{ei:d}")
+        print(f"cut: {cut:.6f} :: {cut:.15f}")
+        print(f"a:   {a:.6f} :: {a:.15f}")
+        print(f"b:   {b:.6f} :: {b:.15f}")
+        print(f"c:   {c:.6f} :: {c:.15f}")
+        print(f"d:   {d:.6f} :: {d:.15f}")
+        print(f"e:   {e:.6f} :: {e:.15f}")
+        print(f"f:   {f:.6f} :: {f:.15f}")
 
     config = ocio.Config.CreateRaw()
 
-    lct = ocio.LogCameraTransform(base=10,
-                                  linSideBreak=[cut, cut, cut],
-                                  logSideSlope=[c, c, c],
-                                  logSideOffset=[d, d, d],
-                                  linSideSlope=[a, a, a],
-                                  linSideOffset=[b, b, b],
-                                  direction=ocio.TRANSFORM_DIR_INVERSE)
+    lct = ocio.LogCameraTransform(
+        base=10,
+        linSideBreak=[cut, cut, cut],
+        logSideSlope=[c, c, c],
+        logSideOffset=[d, d, d],
+        linSideSlope=[a, a, a],
+        linSideOffset=[b, b, b],
+        direction=ocio.TRANSFORM_DIR_INVERSE,
+    )
 
     # consistent with ARRI aces-dev IDT
-    mtx = [[ 6.8020600000000e-01,  2.3613700000000e-01 , 8.3658000000000e-02],
-           [ 8.5415000000000e-02,  1.0174710000000e+00, -1.0288600000000e-01],
-           [ 2.0570000000000e-03, -6.2563000000000e-02,  1.0605060000000e+00]]
+    mtx = [
+        [6.8020600000000e-01, 2.3613700000000e-01, 8.3658000000000e-02],
+        [8.5415000000000e-02, 1.0174710000000e00, -1.0288600000000e-01],
+        [2.0570000000000e-03, -6.2563000000000e-02, 1.0605060000000e00],
+    ]
 
     if clipping:
         # Range Transform to mimic clipping behavior of IDT 1D LUT
@@ -130,38 +139,42 @@ def generate_logc3(ei=800, clipping=True, debug=False):
         rt.setStyle(ocio.RangeStyle.RANGE_CLAMP)
 
         transform_list = [rt, lct, package_matrix(mtx)]
-    
+
     else:
         transform_list = [lct, package_matrix(mtx)]
 
     gt = ocio.GroupTransform(transform_list)
 
-    aces_id = "urn:ampas:aces:transformId:v1.5:IDT.ARRI.Alexa-v3-logC-EI{:d}.a1.v2".format(ei)
+    aces_id = f"urn:ampas:aces:transformId:v1.5:IDT.ARRI.Alexa-v3-logC-EI{ei:d}.a1.v2"
 
     # Write file compliant with new naming convention
-    clf_id = TF_ID_PREFIX + 'ARRI:Input:ARRI_LogC3_EI800_to_ACES2065-1' + TF_ID_SUFFIX
-    
+    clf_id = (
+        TF_ID_PREFIX
+        + "ARRI:Input:ARRI_LogC3_EI800_to_ACES2065-1"
+        + TF_ID_SUFFIX
+    )
+
     fmdg = gt.getFormatMetadata()
     fmdg.setID(clf_id)
-    fmdg.addChildElement('Description', 'ARRI LogC3 (EI800) to ACES2065-1')
-    fmdg.addChildElement('InputDescriptor', 'ARRI LogC3 (EI800)')
-    fmdg.addChildElement('OutputDescriptor', 'ACES2065-1')
-    fmdg.addChildElement('Info', '')
+    fmdg.addChildElement("Description", "ARRI LogC3 (EI800) to ACES2065-1")
+    fmdg.addChildElement("InputDescriptor", "ARRI LogC3 (EI800)")
+    fmdg.addChildElement("OutputDescriptor", "ACES2065-1")
+    fmdg.addChildElement("Info", "")
     info = fmdg.getChildElements()[3]
-    info.addChildElement('ACEStransformID', aces_id)
+    info.addChildElement("ACEStransformID", aces_id)
 
     dest_dir = THIS_DIR / "input"
     if not dest_dir.exists():
         dest_dir.mkdir()
 
-    fname = dest_dir / ("ARRI.LogC3_EI{:d}_to_ACES2065-1".format(ei) + CLF_SUFFIX)
-    gt.write('Academy/ASC Common LUT Format', str(fname), config)
+    fname = dest_dir / (f"ARRI.LogC3_EI{ei:d}_to_ACES2065-1" + CLF_SUFFIX)
+    gt.write("Academy/ASC Common LUT Format", str(fname), config)
 
     return True
 
 
 def generate_logc4(debug=False):
-    """  Generate ARRI LogC4 CLF transforms.
+    """Generate ARRI LogC4 CLF transforms.
 
     Parameters
     ----------
@@ -175,7 +188,8 @@ def generate_logc4(debug=False):
 
     config = ocio.Config.CreateRaw()
 
-    # Parameters as defined in "ARRI LogC4 Logarithmic Color Space Specification - 1st May 2022"
+    # Parameters as defined in
+    # "ARRI LogC4 Logarithmic Color Space Specification - 1st May 2022"
     # Parameters as defined in aces-dev CTL
     a = (2.0**18.0 - 16.0) / 117.45
     b = (1023.0 - 95.0) / 1023.0
@@ -191,63 +205,94 @@ def generate_logc4(debug=False):
     LIN_SIDE_BRK = t
     BASE = 2.0
 
-    # Verify that the default linear slope and offset that will be calculated by OCIO are sufficiently
-    # close to the constants used in the reference CTL.
+    # Verify that the default linear slope and offset that will be calculated by OCIO
+    # are sufficiently close to the constants used in the reference CTL.
     if debug:
-        LINEAR_SLOPE = LOG_SIDE_SLP * LIN_SIDE_SLP / ( (LIN_SIDE_SLP * LIN_SIDE_BRK + LIN_SIDE_OFF) * log(BASE) )
-        LOG_SB = LOG_SIDE_OFF + LOG_SIDE_SLP * log(LIN_SIDE_SLP * LIN_SIDE_BRK + LIN_SIDE_OFF) / log(BASE)
+        LINEAR_SLOPE = (
+            LOG_SIDE_SLP
+            * LIN_SIDE_SLP
+            / ((LIN_SIDE_SLP * LIN_SIDE_BRK + LIN_SIDE_OFF) * log(BASE))
+        )
+        LOG_SB = LOG_SIDE_OFF + LOG_SIDE_SLP * log(
+            LIN_SIDE_SLP * LIN_SIDE_BRK + LIN_SIDE_OFF
+        ) / log(BASE)
         LINEAR_OFFSET = LOG_SB - LINEAR_SLOPE * LIN_SIDE_BRK
-        print(LINEAR_SLOPE, LINEAR_OFFSET, 1./s, -t/s)
-    # This prints 8.803033210331753 0.15895633652241092 8.803033210331753 0.15895633652241087
+        print(LINEAR_SLOPE, LINEAR_OFFSET, 1.0 / s, -t / s)
+    # This prints:
+    #  8.803033210331753 0.15895633652241092 8.803033210331753 0.15895633652241087
     # indicating the default values have sufficient accuracy.
 
-    lct = ocio.LogCameraTransform(base=BASE,
-                                  linSideBreak=[LIN_SIDE_BRK,LIN_SIDE_BRK,LIN_SIDE_BRK],
-                                  logSideSlope=[LOG_SIDE_SLP,LOG_SIDE_SLP,LOG_SIDE_SLP],
-                                  logSideOffset=[LOG_SIDE_OFF,LOG_SIDE_OFF,LOG_SIDE_OFF],
-                                  linSideSlope=[LIN_SIDE_SLP,LIN_SIDE_SLP,LIN_SIDE_SLP],
-                                  linSideOffset=[LIN_SIDE_OFF,LIN_SIDE_OFF,LIN_SIDE_OFF],
-                                  direction=ocio.TRANSFORM_DIR_INVERSE)
+    lct = ocio.LogCameraTransform(
+        base=BASE,
+        linSideBreak=[LIN_SIDE_BRK, LIN_SIDE_BRK, LIN_SIDE_BRK],
+        logSideSlope=[LOG_SIDE_SLP, LOG_SIDE_SLP, LOG_SIDE_SLP],
+        logSideOffset=[LOG_SIDE_OFF, LOG_SIDE_OFF, LOG_SIDE_OFF],
+        linSideSlope=[LIN_SIDE_SLP, LIN_SIDE_SLP, LIN_SIDE_SLP],
+        linSideOffset=[LIN_SIDE_OFF, LIN_SIDE_OFF, LIN_SIDE_OFF],
+        direction=ocio.TRANSFORM_DIR_INVERSE,
+    )
 
-    # Values as defined in "ARRI LogC4 Logarithmic Color Space Specification - 1st May 2022"
-    mtx = [[  0.7509573628,  0.1444227867,  0.1046198505],
-           [  0.0008218371,  1.0073975849, -0.0082194220],
-           [ -0.0004999521, -0.0008541772,  1.0013541294]]
+    # Values as defined in:
+    # "ARRI LogC4 Logarithmic Color Space Specification - 1st May 2022"
+    mtx = [
+        [0.7509573628, 0.1444227867, 0.1046198505],
+        [0.0008218371, 1.0073975849, -0.0082194220],
+        [-0.0004999521, -0.0008541772, 1.0013541294],
+    ]
 
     gt = ocio.GroupTransform([lct, package_matrix(mtx)])
 
-    clf_id = TF_ID_PREFIX + 'ARRI:Input:ARRI_LogC4_to_ACES2065-1' + TF_ID_SUFFIX
+    clf_id = (
+        TF_ID_PREFIX + "ARRI:Input:ARRI_LogC4_to_ACES2065-1" + TF_ID_SUFFIX
+    )
 
     # Consistent with CTL in aces-dev PR:
-    aces_id = 'urn:ampas:aces:transformId:v1.5:IDT.ARRI.LogC4.a1.v1'
+    aces_id = "urn:ampas:aces:transformId:v1.5:IDT.ARRI.LogC4.a1.v1"
 
     fmdg = gt.getFormatMetadata()
     fmdg.setID(clf_id)
-    fmdg.addChildElement('Description', 'ARRI LogC4 to ACES2065-1')
-    fmdg.addChildElement('InputDescriptor', 'ARRI LogC4')
-    fmdg.addChildElement('OutputDescriptor', 'ACES2065-1')
-    fmdg.addChildElement('Info', '')
+    fmdg.addChildElement("Description", "ARRI LogC4 to ACES2065-1")
+    fmdg.addChildElement("InputDescriptor", "ARRI LogC4")
+    fmdg.addChildElement("OutputDescriptor", "ACES2065-1")
+    fmdg.addChildElement("Info", "")
     info = fmdg.getChildElements()[3]
-    info.addChildElement('ACEStransformID', aces_id)
+    info.addChildElement("ACEStransformID", aces_id)
 
     dest_dir = THIS_DIR / "input"
     if not dest_dir.exists():
         dest_dir.mkdir()
 
     fname = dest_dir / ("ARRI.LogC4_to_ACES2065-1" + CLF_SUFFIX)
-    gt.write('Academy/ASC Common LUT Format', str(fname), config)
+    gt.write("Academy/ASC Common LUT Format", str(fname), config)
 
     return True
 
 
 def package_matrix(mtx):
-    """ Package a 3x3 matrix as an OCIO Matrix transform. """
+    """Package a 3x3 matrix as an OCIO Matrix transform."""
 
-    coefs4x4 = [0.0,0.0,0.0,0.0,  0.0,0.0,0.0,0.0,  0.0,0.0,0.0,0.0,  0.0,0.0,0.0,1.0]
+    coefs4x4 = [
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        1.0,
+    ]
     coefs4x4[0:3] = mtx[0]
     coefs4x4[4:7] = mtx[1]
     coefs4x4[8:11] = mtx[2]
-    offs = [0.0,0.0,0.0,0.0]
+    offs = [0.0, 0.0, 0.0, 0.0]
     direc = ocio.TRANSFORM_DIR_FORWARD
     mtxOp = ocio.MatrixTransform(coefs4x4, offs, direc)
     return mtxOp
@@ -259,5 +304,5 @@ def main():
     return 0
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     sys.exit(main())
