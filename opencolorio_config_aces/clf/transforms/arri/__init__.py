e706efdea9bc1ae54cdea788502ee794ae55bfda
scoopxyz <sean@seancooper.xyz>
2022-06-07 17:59:30 +0200

    add module to avoid name collision
    Signed-off-by: scoopxyz <sean@seancooper.xyz>

---
+++ opencolorio_config_aces/clf/transforms/arri/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright Contributors to the OpenColorIO Project.
+
+from .generate import (
+    generate_logc3,
+    generate_logc4
+)
+
+__all__ = [
+    "generate_logc3",
+    "generate_logc4",
+]
