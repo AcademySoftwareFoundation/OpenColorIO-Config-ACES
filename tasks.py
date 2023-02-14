7c24314ad2d04e13969dc39b6e7983b9426efbb7
Thomas Mansencal <thomas.mansencal@gmail.com>
2023-02-15 07:26:05 +1300

    Implement initial support for Python 3.11.
    Signed-off-by: Thomas Mansencal <thomas.mansencal@gmail.com>

--- tasks.py
+++ tasks.py
@@ -10,7 +10,6 @@ from __future__ import annotations
 import os
 
 import requests
-from invoke import Context, task
 from invoke.exceptions import Failure
 from pathlib import Path
 import opencolorio_config_aces
@@ -24,6 +23,13 @@ from opencolorio_config_aces.config.studio.generate.config import (
     URL_EXPORT_TRANSFORMS_MAPPING_FILE_STUDIO,
 )
 from opencolorio_config_aces.utilities import google_sheet_title, message_box
+
+import inspect
+
+if not hasattr(inspect, "getargspec"):
+    inspect.getargspec = inspect.getfullargspec
+
+from invoke import Context, task
 
 __author__ = "OpenColorIO Contributors"
 __copyright__ = "Copyright Contributors to the OpenColorIO Project."
@@ -84,31 +90,6 @@ PYPI_PACKAGE_NAME = "opencolorio-config-aces"
 ORG = "aswf"
 
 CONTAINER = PYPI_PACKAGE_NAME
-
-
-def _patch_invoke_annotations_support():
-    """See https://github.com/pyinvoke/invoke/issues/357."""
-
-    import invoke
-    from unittest.mock import patch
-    from inspect import getfullargspec, ArgSpec
-
-    def patched_inspect_getargspec(function):
-        spec = getfullargspec(function)
-        return ArgSpec(*spec[0:4])
-
-    org_task_argspec = invoke.tasks.Task.argspec
-
-    def patched_task_argspec(*args, **kwargs):
-        with patch(
-            target="inspect.getargspec", new=patched_inspect_getargspec
-        ):
-            return org_task_argspec(*args, **kwargs)
-
-    invoke.tasks.Task.argspec = patched_task_argspec
-
-
-_patch_invoke_annotations_support()
 
 
 @task
