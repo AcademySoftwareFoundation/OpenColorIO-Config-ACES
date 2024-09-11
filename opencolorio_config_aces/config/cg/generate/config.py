03edc4b54078a7033eb0d06ea017b5c90f9ab2d3
Thomas Mansencal <thomas.mansencal@gmail.com>
2024-09-11 19:01:53 +1200

    Insert the *Un-tone-mapped* view transform at the beginning of the view transforms list for *OpenColorIO* profile lesser or equal to 2.3.
    Signed-off-by: Thomas Mansencal <thomas.mansencal@gmail.com>

--- opencolorio_config_aces/config/cg/generate/config.py
+++ opencolorio_config_aces/config/cg/generate/config.py
@@ -1234,7 +1234,6 @@ if __name__ == "__main__":
             dependency_versions=dependency_versions,
             additional_data=True,
         )
-
         # TODO: Pickling "PyOpenColorIO.ColorSpace" fails on early "PyOpenColorIO"
         # versions.
         try:
@@ -1243,3 +1242,16 @@ if __name__ == "__main__":
             )
         except TypeError as error:
             logging.critical(error)
+
+        if dependency_versions.ocio.minor <= 3:
+            config = ocio.Config.CreateFromFile(  # pyright:ignore
+                str(build_directory / config_basename)
+            )
+            view_transforms = list(config.getViewTransforms())
+            view_transforms = [view_transforms[-1], *view_transforms[:-1]]
+            config.clearViewTransforms()
+            for view_transform in view_transforms:
+                config.addViewTransform(view_transform)
+
+            with open(build_directory / config_basename, "w") as file:
+                file.write(config.serialize())
